19b1d45031f7371d48193f979fb01b8d725797ea
Daniil Vakhlov <dgvakhlov@avito.ru>
2024-04-25 10:18:52 +0300

    lab2: fix data_loading script - no data directory

--- lab2/data_loading.py
+++ lab2/data_loading.py
@@ -1,5 +1,8 @@
+import os
+
 import pandas as pd
 
 if __name__ == '__main__':
+    os.makedirs("data", exist_ok=True)
     df = pd.read_csv('https://raw.githubusercontent.com/djvue/ml-models-mirror/main/house-prices-advanced-regression-techniques/train.csv')
     df.to_csv("data/data.csv", index=False)
