5e07980fea15c0af327acf8d267944b41449522f
Daniil Vakhlov <dgvakhlov@avito.ru>
2024-04-29 20:54:00 +0300

    lab4: add test titanic dataset

--- lab4/load_dataset.py
+++ lab4/load_dataset.py
@@ -5,4 +5,4 @@ titanic_train, titanic_test = titanic()
 
 os.makedirs("data", exist_ok=True)
 titanic_train.to_csv("data/titanic_train.csv", index=False)
-titanic_test.to_csv("data/titanic_train.csv", index=False)
+titanic_test.to_csv("data/titanic_test.csv", index=False)
